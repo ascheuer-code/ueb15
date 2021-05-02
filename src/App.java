6270a183f01924605dcbe1d2e6d95a825df9a28d
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-05-02 10:31:24 +0200

    finaly.....

--- src/App.java
+++ src/App.java
@@ -1,8 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class App {
 
@@ -21,30 +25,32 @@ public class App {
 
     public void start(String[] args) throws FileNotFoundException {
 
-        for (String string : args) {
-            System.out.println(palindromiterativ.isPalindrom(string));
-            System.out.println(palindromrekursiv.isPalindrom(string));
+        final String myFileName = args[0];
+        File file = new File(myFileName);
+        if (file.exists()) {
+            List<String> list = getFileContent(file);
 
-            // String file = getFileContent(filename);
-            // if (file != null) {
-            // while (getFileContent(filename) != null) {
-            // palindrom.isPalindrom(file);
-            // }
-            // } else {
-            // }
+            list.forEach(p -> System.out.println(palindromiterativ.isPalindrom(p)));
+            list.forEach(p -> System.out.println(palindromrekursiv.isPalindrom(p)));
+        } else {
+            for (String string : args) {
+                System.out.println(palindromiterativ.isPalindrom(string));
+                System.out.println(palindromrekursiv.isPalindrom(string));
+            }
         }
 
     }
 
-    public static String getFileContent(String filename) {
-        final File file = new File(filename);
-        if (file.exists()) {
+    public static List<String> getFileContent(File file) {
+        {
             try {
-                return Files.readString(Paths.get(file.getAbsolutePath()));
+                return Files.readAllLines(Paths.get(file.getAbsolutePath()));
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
         return null;
     }
 
